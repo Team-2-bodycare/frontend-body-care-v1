2ff88b24ae086ee1a9d541d320a35775e228a739
GregorioGrGeOtOl <gregoriogrgeotavia@hotmail.com>
2023-03-13 21:27:15 -0300

    chore: add route signin

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,16 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "../pages/home/Home";
+import { Principal } from "../pages/principal/Principal";
+import { Signin } from "../pages/signin/Signin";
 import { PsicologosRegister } from "../pages/signupPsicologo/SignupPsicologo";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/psicologos" element={<PsicologosRegister />} />
+      <Route path="/signup" element={<PsicologosRegister />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/principal" element={<Principal />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
